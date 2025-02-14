a606d00420c5a566478aecbc500f5a11446599c7
zakaria_elaoufi <zakariaaoufi8@gmail.com>
2025-02-15 00:29:49 +0100

    add logistic regression from scratch, with some test

--- logistic-regression-from-scratch.py
+++ logistic-regression-from-scratch.py
@@ -1,6 +1,8 @@
 import numpy as np
 import pandas as pd
 from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
 
 
 def sigmoid(X, coef):
@@ -18,8 +20,12 @@ def gradient_ascent(X, y, coef, learning_rate, epochs):
 def model_fit(X, y):
     X_bias = np.c_[np.ones((X.shape[0], 1)), X]  # Add bias term
     coef = np.random.normal(0, 1, X_bias.shape[1])  # Initialize coefficients
-    final_coef = gradient_ascent(X_bias, y, coef, learning_rate=0.05, epochs=1000)
+    final_coef = gradient_ascent(X_bias, y, coef, learning_rate=0.05, epochs=500)
     return final_coef
+
+def predict(X, coef):
+    X_bias = np.c_[np.ones((X.shape[0], 1)), X]
+    return np.round(sigmoid(X_bias, coef))
 
 
 # Load and clean data
@@ -33,6 +39,27 @@ y = data['TenYearCHD']
 scaler = StandardScaler()
 X_scaled = scaler.fit_transform(X)  # Standardize the features
 
+X_train, X_test, y_train, y_test = train_test_split(X_scaled, y, test_size=0.2, random_state=42)
+
 # Fit the model
-coef = model_fit(X_scaled, y)
+coef = model_fit(X_train, y_train)
 print(coef)
+
+y_pred = predict(X_test, coef)
+
+#################################################"
+lr = LogisticRegression()
+
+lr.fit(X_scaled, y)
+
+print(lr.coef_, lr.intercept_)
+
+y_pred2 = lr.predict(X_test)
+
+###############################################
+from sklearn.metrics import precision_score
+
+print(precision_score(y_test, y_pred))
+print("tets")
+print(precision_score(y_test, y_pred2))
+
